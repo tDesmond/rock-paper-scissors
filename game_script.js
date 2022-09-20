7beb1c3880ace7987d1d63e319695074719ca53f
tDesmond <timmydesmond95@gmail.com>
2022-09-20 16:49:21 +0100

    Created a functional game. Best to 3 and the game restarts when someone wins

--- game_script.js
+++ game_script.js
@@ -9,6 +9,9 @@ const PAPER_BUTTON = document.querySelector("#paper-btn")
 const SCISSORS_BUTTON = document.querySelector("#scissors-btn")
 const PLAY_BUTTON = document.querySelector("#play-btn")
 
+const PLAYER_SCORE = document.querySelector(".player-score")
+const COMPUTER_SCORE = document.querySelector(".computer-score")
+
 
 let computerSelection, playerSelection, playerScore=0, computerScore=0
 
@@ -19,8 +22,7 @@ function getComputerChoice(){
     return CHOICES[radomInt()]
 }
 
-function playRound(computer, player){
-
+function checkWinner(computer, player){
     if (computer == player) return "T'is a draw!"
     if (computer == "ROCK"){
         if (player == "SCISSORS") {
@@ -54,13 +56,35 @@ function setPlayerChoice(choice){
     PLAYER_CHOICE.textContent = playerSelection
 }
 
-function game(){
+function updateScore(){
+    PLAYER_SCORE.textContent = playerScore
+    COMPUTER_SCORE.textContent = computerScore
+}
 
+function isGameOver(){
+    console.log((playerScore >= 3))
+    console.log((computerScore >= 3))
+    return ((playerScore >= 3) || (computerScore >= 3))
+}
+
+function playRound(computer, player){
+    RESULT_TEXT.textContent = checkWinner(computer, player)
+    updateScore()
+    if(isGameOver()){
+        alert(`Game over
+            You: ${playerScore}
+            Compter: ${computerScore}`)
+        playerScore = 0
+        computerScore = 0
+        updateScore()
+    }
+}
+function game(){
     ROCK_BUTTON.onclick = () => setPlayerChoice("ROCK")
     PAPER_BUTTON.onclick = () => setPlayerChoice("PAPER")
     SCISSORS_BUTTON.onclick = () => setPlayerChoice("SCISSORS")
 
-    PLAY_BUTTON.onclick = () => RESULT_TEXT.textContent = (playRound(getComputerChoice(), playerSelection))
+    PLAY_BUTTON.onclick = () => (playRound(getComputerChoice(), playerSelection))
 }
 
 game()
